839282275d742ac1d14f433a5f0fb0d196cf84c9
bielcst <gabrielgarocs@gmail.com>
2020-12-19 20:40:16 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ def save():
 
 atexit.register(save)
 #setting api things up
-consumer_key = 'pmPjjM2gaIxd2v6yYp6kR2y7q'
-consumer_secret = 'B0kzQP2rZFtC8mOQ4Z8W9cq1SobR62Xd4nL0J1DsaogFuqMi7n'
-access_token = '1163671052670705664-SUCpXnrUEiWeJgGJYYciPURjgDa3ax'
-access_token_secret = '5EwfbFk5g3vn97ZyI5lxW3KU5IJBeoHxKeCkRRex8kFKI'
+consumer_key = 'twitter_consumer_key'
+consumer_secret = 'twitter_consumer_secret'
+access_token = 'twitter_access_token'
+access_token_secret = 'twitter_secret_access_token'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-watcher = RiotWatcher('RGAPI-a65f4ad6-a97c-40b4-a637-821c631c0260')
+watcher = RiotWatcher('riot_api_key')
 criminoso = watcher.summoner.by_name('br1', 'megatronn')
 mensagem1 = 'Megatronn está jogando, coloque na fila antes do jogo dele acabar.'
 mensagem2 = 'O jogo do Megatronn acabou agora, cuidado ao entrar na fila.'
